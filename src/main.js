c18d0e7d5f7a53620eaf9fcea98611f4d2af3a02
Sergey Lugovykh <sslugovykh@gmail.com>
2017-07-17 21:21:51 +0400

    Simple style changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
 let newCard = document.createElement('ui-card');
-newCard.className = 'info';
-newCard.innerHTML = '<h2 slot="title">Hello World!</h2><p>Привет мир!</p>';
+newCard.insertAdjacentHTML(
+  'beforeEnd',
+  '<h2 slot="title">Hello World!</h2>'
+);
+newCard.insertAdjacentHTML(
+  'beforeEnd',
+  '<span slot="date">2017.07.07</span>'
+);
+newCard.insertAdjacentHTML(
+  'beforeEnd',
+  '<p>Привет мир!</p>'
+);
 document.body.append(newCard);
 
 let newCard1 = document.createElement('ui-card');
-newCard1.className = 'warning';
 newCard1.textContent = 'Hell World!';
 document.body.append(newCard1);
